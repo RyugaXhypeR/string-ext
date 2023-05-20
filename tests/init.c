1cc3293c27d7223f8629a2dead22dac93ff9758c
RyugaXhypeR <ryugashyper@gmail.com>
2023-05-20 16:10:18 +0530

    Change: moving inputs for test cases inside function.

--- tests/init.c
+++ tests/init.c
@@ -11,23 +11,26 @@ compare_string(StringT *string, const char *str) {
 }
 
 void
-test_string_new(ssize_t size) {
+test_string_new() {
+    ssize_t size = 10;
     StringT *string = String_new(size);
     assert(string->allocated == size);
+
     String_free(string);
 }
 
 void
-test_string_from(const char *str) {
+test_string_from() {
+    const char *str = "Hello, World!";
     StringT *string = String_from(str);
     assert(string->length == strlen(str));
+
     compare_string(string, str);
     String_free(string);
 }
 
 int
 main() {
-    test_string_new(10);
-    test_string_from("Hello World!");
-    return 0;
+    test_string_new();
+    test_string_from();
 }
