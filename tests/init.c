698fd7bbd325a354c0faca387f90d2baf2cda55d
RyugaXhypeR <ryugashyper@gmail.com>
2023-05-20 16:50:15 +0530

    Change: moved `tests/utils.h` to `include/`

--- tests/init.c
+++ tests/init.c
@@ -1,7 +1,7 @@
 /// Tests the initlialization of the `StringT` object.
 
 #include "../include/string.h"
-#include "utils.h"
+#include "../include/utils.h"
 
 #include <assert.h>
 #include <string.h>
