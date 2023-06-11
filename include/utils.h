2264ff371fe9afc40dcf445cc0a93fe54bfab5f4
RyugaXhypeR <ryugashyper@gmail.com>
2023-06-11 20:04:14 +0530

    Addition: two macros to free multiple `StringT` and `StringIteratorT`s.

--- include/utils.h
+++ include/utils.h
@@ -3,7 +3,17 @@
 
 #include "../include/string.h"
 
+#include <stdarg.h>
 #include <stdio.h>
+
+#define STRING_FREE_MULTIPLE(...)                                                        \
+    string_free_multiple(__NUM_ARGS(StringT *, __VA_ARGS__), __VA_ARGS__)
+#define STRING_ITERATOR__FREE_MULTIPLE(...)                                              \
+    string_iterator_free_multiple(__NUM_ARGS(StringIteratorT *, __VA_ARGS__), __VA_ARGS__)
+
+
+void string_free_multiple(int num_args, ...);
+void string_iterator_free_multiple(int num_args, ...);
 
 inline int
 string_t_equals(StringT *str1, StringT *str2) {
@@ -33,6 +43,32 @@ log_result(const char *method_name, int result) {
 inline int
 string_index_equal(const StringIndexT idx1, const StringIndexT idx2) {
     return idx1.start == idx2.start && idx1.stop == idx2.stop && idx1.step == idx2.step;
+}
+
+inline void
+string_free_multiple(int num_args, ...) {
+    va_list args;
+    va_start(args, num_args);
+
+    while (num_args--) {
+        StringT *str = va_arg(args, StringT *);
+        String_free(str);
+    }
+
+    va_end(args);
+}
+
+inline void
+string_iterator_free_multiple(int num_args, ...) {
+    va_list args;
+    va_start(args, num_args);
+
+    while (num_args--) {
+        StringIteratorT *iter = va_arg(args, StringIteratorT *);
+        StringIterator_free(iter);
+    }
+
+    va_end(args);
 }
 
 #endif  // UNIT_TEST_UTILS_H
