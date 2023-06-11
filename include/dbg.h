9849826c2a7a066f5411e4df1e0d88730b77df13
RyugaXhypeR <ryugashyper@gmail.com>
2023-06-11 10:57:04 +0530

    Fix: Wrapped macros around do while and added `ERR` macro.

--- include/dbg.h
+++ include/dbg.h
@@ -3,17 +3,21 @@
 
 #ifdef DEBUG
 #define DBG(...)                                                                         \
-    printf("[%s:%d] ", __FILE__, __LINE__);                                              \
-    printf(__VA_ARGS__);                                                                 \
-    puts("");
+    do {                                                                                 \
+        printf("[%s:%d] ", __FILE__, __LINE__);                                          \
+        printf(__VA_ARGS__);                                                             \
+        puts("");                                                                        \
+    } while (0)
 #else
 #define DBG(...)
 
 #define ERR(...)                                                                         \
-    fprintf(stderr, "[%s:%d] ", __FILE__, __LINE__);                                     \
-    fprintf(stderr, __VA_ARGS__);                                                        \
-    fprintf(stderr, "\n");                                                               \
-    exit(EXIT_FAILURE);
+    do {                                                                                 \
+        fprintf(stderr, "[%s:%d] ", __FILE__, __LINE__);                                 \
+        fprintf(stderr, __VA_ARGS__);                                                    \
+        fprintf(stderr, "\n");                                                           \
+        exit(EXIT_FAILURE);                                                              \
+    } while (0)
 #endif
 
 #endif /* DBG_H */
